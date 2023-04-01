1782501d75c42cfa38822140dc78b62c7f232207
Emilija Dunoska <emilijadunoska@gmail.com>
2023-04-02 00:07:43 +0200

    Login & Register UI

--- App.js
+++ App.js
@@ -3,8 +3,13 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import OnboardingScreen from './src/screens/OnboardingScreen';
-import HomeScreen from './src/screens/HomeScreen';
+import HomeScreen from './src/screens/ChatScreen';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import ChatScreen from './src/screens/ChatScreen';
+
 const Stack = createStackNavigator();
 
 const App = () => {
@@ -33,7 +38,10 @@ const App = () => {
               component={OnboardingScreen}
             />
           )}
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="ChatScreen" component={ChatScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     )
--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   SafeAreaView,
   Image,
@@ -9,55 +9,56 @@ import {
   StatusBar,
   TouchableOpacity,
   Dimensions,
-} from 'react-native';
+} from "react-native";
+import Colors from "../constants/Colors";
+import FontSize from "../constants/FontSize";
+import Spacing from "../constants/Spacing";
 
-const {width, height} = Dimensions.get('window');
-
-const COLORS = {primary: '#282534', white: '#fff'};
+const { width, height } = Dimensions.get("window");
 
 const slides = [
   {
     id: "1",
     image: require("../images/image1.png"),
-    title: "A safe space to share and heal",
+    title: "A safe space \n to share and heal",
     subtitle: "Your mental health journey begins here.",
   },
   {
     id: "2",
     image: require("../images/image2.png"),
-    title: "Take some time to travel within",
+    title: "Take some time \n to travel within",
     subtitle: "Mental health is not a destination, but a journey!",
   },
   {
     id: "3",
     image: require("../images/image3.png"),
-    title: "A healthy mind is an asset",
+    title: "A healthy mind \n is an asset",
     subtitle: "Chat your way to a better you!",
   },
-  
 ];
 
-const Slide = ({item}) => {
+const Slide = ({ item }) => {
   return (
-    <View style={{alignItems: 'center'}}>
-       <Image
-        source={item.image}
-        style={{height: '80%', width, resizeMode: 'contain'}}
-      />
-      <View style={{alignItems: 'center'}}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.subtitle}>{item.subtitle}</Text>
+    <SafeAreaView>
+      <View style={{ alignItems: "center", marginTop: Spacing * 2 }}>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.subtitle}>{item.subtitle}</Text>
+        </View>
+        <Image
+          source={item.image}
+          style={{ height: "80%", width, resizeMode: "contain" }}
+        />
       </View>
-     
-    </View>
+    </SafeAreaView>
   );
 };
 
-const OnboardingScreen = ({navigation}) => {
+const OnboardingScreen = ({ navigation }) => {
   const [currentSlideIndex, setCurrentSlideIndex] = React.useState(0);
   const ref = React.useRef();
-  
-  const updateCurrentSlideIndex = e => {
+
+  const updateCurrentSlideIndex = (e) => {
     const contentOffsetX = e.nativeEvent.contentOffset.x;
     const currentIndex = Math.round(contentOffsetX / width);
     setCurrentSlideIndex(currentIndex);
@@ -67,7 +68,7 @@ const OnboardingScreen = ({navigation}) => {
     const nextSlideIndex = currentSlideIndex + 1;
     if (nextSlideIndex != slides.length) {
       const offset = nextSlideIndex * width;
-      ref?.current.scrollToOffset({offset});
+      ref?.current.scrollToOffset({ offset });
       setCurrentSlideIndex(currentSlideIndex + 1);
     }
   };
@@ -75,7 +76,7 @@ const OnboardingScreen = ({navigation}) => {
   const skip = () => {
     const lastSlideIndex = slides.length - 1;
     const offset = lastSlideIndex * width;
-    ref?.current.scrollToOffset({offset});
+    ref?.current.scrollToOffset({ offset });
     setCurrentSlideIndex(lastSlideIndex);
   };
 
@@ -84,73 +85,87 @@ const OnboardingScreen = ({navigation}) => {
       <View
         style={{
           height: height * 0.25,
-          justifyContent: 'space-between',
+          justifyContent: "space-between",
           paddingHorizontal: 20,
-        }}>
+        }}
+      >
         <View
           style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
+            flexDirection: "row",
+            justifyContent: "center",
             marginTop: 20,
-          }}>
+          }}
+        >
           {slides.map((_, index) => (
             <View
               key={index}
               style={[
                 styles.indicator,
                 currentSlideIndex == index && {
-                  backgroundColor: COLORS.primary,
+                  backgroundColor: Colors.primary,
                   width: 25,
                 },
               ]}
             />
           ))}
         </View>
-        <View style={{marginBottom: 20}}>
+        <View style={{ marginBottom: 20 }}>
           {currentSlideIndex == slides.length - 1 ? (
-            <View style={{height: 50}}>
+            <View>
               <TouchableOpacity
-                style={styles.btn}
-                onPress={() => navigation.replace('HomeScreen')}>
-                <Text style={{fontWeight: 'bold', fontSize: 15, color: COLORS.white}}>
+                style={[styles.getstartedBtn]}
+                onPress={() => navigation.navigate("WelcomeScreen")}
+              >
+                <Text
+                  style={{
+                    color: Colors.onPrimary,
+                    fontWeight: 'bold',
+                    textAlign: "center",
+                    fontSize: FontSize.medium,
+                  }}
+                >
                   GET STARTED
                 </Text>
               </TouchableOpacity>
             </View>
           ) : (
-            <View style={{flexDirection: 'row'}}>
+            <View style={{ flexDirection: "row" }}>
               <TouchableOpacity
                 activeOpacity={0.8}
                 style={[
                   styles.btn,
                   {
-                    borderColor: COLORS.primary,
+                    borderColor: Colors.primary,
                     borderWidth: 1,
-                    backgroundColor: 'transparent',
-                    color: COLORS.primary,
+                    backgroundColor: "transparent",
+                    color: Colors.primary,
                   },
                 ]}
-                onPress={skip}>
+                onPress={skip}
+              >
                 <Text
                   style={{
-                    fontWeight: 'bold',
-                    fontSize: 15,
-                    color: COLORS.primary,
-                  }}>
+                    fontWeight: "bold",
+                    fontSize: FontSize.medium,
+                    color: Colors.primary,
+                  }}
+                >
                   SKIP
                 </Text>
               </TouchableOpacity>
-              <View style={{width: 15}} />
+              <View style={{ width: 15 }} />
               <TouchableOpacity
                 activeOpacity={0.8}
                 onPress={goToNextSlide}
-                style={styles.btn}>
+                style={styles.btn}
+              >
                 <Text
                   style={{
-                    fontWeight: 'bold',
-                    fontSize: 15,
-                    color: COLORS.white
-                  }}>
+                    fontWeight: "bold",
+                    fontSize: FontSize.medium,
+                    color: Colors.white,
+                  }}
+                >
                   NEXT
                 </Text>
               </TouchableOpacity>
@@ -162,19 +177,20 @@ const OnboardingScreen = ({navigation}) => {
   };
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
-      <StatusBar backgroundColor={COLORS.white} />
+    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.white }}>
+      <StatusBar backgroundColor={Colors.white} />
+
       <FlatList
         pagingEnabled
         ref={ref}
         onMomentumScrollEnd={updateCurrentSlideIndex}
-        contentContainerStyle={{height: height * 0.65}}
+        contentContainerStyle={{ height: height * 0.65 }}
         showsHorizontalScrollIndicator={false}
         horizontal
         data={slides}
-       
-        renderItem={({item}) => <Slide item={item} />}
+        renderItem={({ item }) => <Slide item={item} />}
       />
+
       <Footer />
     </SafeAreaView>
   );
@@ -182,39 +198,60 @@ const OnboardingScreen = ({navigation}) => {
 
 const styles = StyleSheet.create({
   subtitle: {
-    color: COLORS.primary,
+    color: Colors.primary,
     fontSize: 14,
     marginTop: 10,
-    maxWidth: '70%',
-    textAlign: 'center',
+    maxWidth: "70%",
+    textAlign: "center",
     lineHeight: 23,
   },
   title: {
-    color: COLORS.primary,
-    fontSize: 24,
-    fontWeight: 'bold',
+    color: Colors.primary,
+    fontSize: FontSize.xxLarge,
+    fontWeight: "bold",
     marginTop: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
   image: {
-    height: '100%',
-    width: '100%',
-    resizeMode: 'contain',
+    height: "100%",
+    width: "100%",
+    resizeMode: "contain",
   },
   indicator: {
     height: 2.5,
     width: 10,
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
     marginHorizontal: 3,
     borderRadius: 2,
   },
   btn: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary,
+    padding: Spacing * 2,
     flex: 1,
-    height: 50,
-    borderRadius: 5,
-    backgroundColor: COLORS.primary,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginVertical: Spacing * 3,
+    borderRadius: Spacing,
+    shadowColor: Colors.primary,
+    shadowOffset: {
+      width: 0,
+      height: Spacing,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: Spacing,
+  },
+  getstartedBtn: {
+    padding: Spacing * 2,
+    backgroundColor: Colors.primary,
+    marginVertical: Spacing * 3,
+    borderRadius: Spacing,
+    shadowColor: Colors.primary,
+    shadowOffset: {
+      width: 0,
+      height: Spacing,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: Spacing,
   },
 });
 
